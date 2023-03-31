0ac5e413a82d93ab397cc5239cc9462bb3cc60f7
Зайнулин Фаиз <uvelir.f.z@mail.ru>
2023-03-31 21:15:52 +0300

    add Ex2

---
+++ Ex2/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая принимает на вход 3 числа и выдает максимальное из этих чисел
+Console.Clear();
+
+Console.Write("Введите первое число:");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число:");
+int numberB = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число:");
+int numberC = int.Parse(Console.ReadLine());
+
+int max = numberA;
+
+if(max < numberB)
+{
+    max = numberB;
+}
+if (max < numberC)
+{
+    max = numberC;
+}
+
+Console.WriteLine($"{max} большее из чисел");
