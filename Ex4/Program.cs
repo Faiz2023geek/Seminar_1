df3dde6cc1869785468e23dabc913b93000e6aa5
Зайнулин Фаиз <uvelir.f.z@mail.ru>
2023-03-31 21:37:19 +0300

    add Ex4

---
+++ Ex4/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N.
+Console.Clear();
+
+Console.Write("Введите число:");
+int number = int.Parse(Console.ReadLine());
+int count = 2;
+
+while (count <= number)
+{
+    Console.Write($"{count}");
+    count += 2;
+}
